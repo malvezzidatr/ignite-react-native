f6e854ef458ffa21bcfe67c755d3b994b05c7d25
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-07-20 11:57:40 -0300

    Iniciando a criação do header

--- chapter II/gofinances/src/screens/Dashboard/index.tsx
+++ chapter II/gofinances/src/screens/Dashboard/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {  } from '@expo/vector-icons';
+
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/60995617?v=4'}}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Caio</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 };
