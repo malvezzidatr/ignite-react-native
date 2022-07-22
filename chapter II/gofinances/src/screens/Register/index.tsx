bb0b385e3f4a874f6e7a0ae9d874431d50f370e1
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-07-22 09:48:44 -0300

    Criando botões de income e outcome

--- chapter II/gofinances/src/screens/Register/index.tsx
+++ chapter II/gofinances/src/screens/Register/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
     Container,
     Header,
     Title,
-    Form
+    Form,
+    Fields,
+    TransactionsTypes
 } from './styles';
 
 import { Input } from '../../components/Forms/Input';
+import { Button } from '../../components/Forms/Button';
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
 
 export function Register() {
+    const [transactionType, setTransactionType] = useState();
+
+    function handleTransactionsTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
     return (
         <Container>
             <Header>
@@ -16,12 +26,29 @@ export function Register() {
             </Header>
 
             <Form>
-                <Input
-                    placeholder='Nome'
-                />
-                <Input
-                    placeholder='Preço'
-                />
+                <Fields>
+                    <Input
+                        placeholder='Nome'
+                    />
+                    <Input
+                        placeholder='Preço'
+                    />
+                    <TransactionsTypes>
+                        <TransactionTypeButton
+                            type='up'
+                            title='Income'
+                            isActive={transactionType === 'up'}
+                            onPress={() => handleTransactionsTypeSelect('up')}
+                        />
+                        <TransactionTypeButton
+                            type='down'
+                            title='Outcome'
+                            isActive={transactionType === 'down'}
+                            onPress={() => handleTransactionsTypeSelect('down')}
+                        />
+                    </TransactionsTypes>
+                </Fields>
+                <Button title='Enviar' />
             </Form>
         </Container>
     );
