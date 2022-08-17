9b1c76c59504b070fd84b1b4b43600d92a62bd6b
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-08-17 12:31:00 -0300

    Iniciando a tela de login usando novas libs

--- chapter II/gofinances/App.tsx
+++ chapter II/gofinances/App.tsx
@@ -14,6 +14,7 @@ import {
 import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -30,7 +31,7 @@ export default function App() {
         <ThemeProvider theme={ theme }>
             <NavigationContainer>
                 <StatusBar barStyle="light-content"/>
-                <AppRoutes />
+                <SignIn />
             </NavigationContainer>
         </ThemeProvider>
     )
